e9c2b6e14b5dfb47298853f632b022f4650f4fcd
qiuyangfeng <qq695702782@gmail.com>
2020-04-26 16:25:35 +0800

    ubuntu16.04修改计算机名 修改dhcp为固定IP

--- renameip.sh
+++ renameip.sh
@@ -7,9 +7,14 @@ read -p "(1).确认  (2).重设  (3).退出  请输入数字选择 > " CONFIRM
 case $CONFIRM in
   1) if [ ! -z $NEWIP ] && [ ! -z $NEWGATEWAY ];then
           sudo sed -i "s/dhcp/static/" /etc/network/interfaces && echo "    address $NEWIP"|sudo tee -a /etc/network/interfaces
-          echo "    netmask 24"|sudo tee -a /etc/network/interfaces && echo "    gateway $NEWGATEWAY"|sudo tee -a /etc/network/interfaces
-	  echo "    dns-nameservers 172.16.1.2 223.6.6.6"|sudo tee -a /etc/network/interfaces && sudo timedatectl set-timezone Asia/Shanghai
-          echo "$NEWIP callcenter-$A-$B"|sudo tee -a /etc/hosts && echo "callcenter-$A-$B"|sudo tee /etc/hostname && sudo reboot
+          echo "    netmask $NEWNETMASK"|sudo tee -a /etc/network/interfaces && echo "    gateway $NEWGATEWAY"|sudo tee -a /etc/network/interfaces
+	  echo "    dns-nameservers $NEWDNS"|sudo tee -a /etc/network/interfaces && sudo timedatectl set-timezone Asia/Shanghai
+          echo "$NEWIP $NEWNAME-$A-$B"|sudo tee -a /etc/hosts && echo "$NEWNAME-$A-$B"|sudo tee /etc/hostname && echo "即将重启系统!!"
+          for i in $(seq 5 -1 1)
+          do
+            sleep 1 && echo $i
+          done
+          sudo reboot
      else
           echo -e "\n错误的IP地址或网关地址,请重新执行脚本！" && exit 1
      fi
@@ -27,12 +32,15 @@ esac
 # 提示输入IP和网关地址
 setIphost()
 {
-read -p "请输入你要配置本机的IP地址 > " NEWIP
-read -p "请输入你要配置本机的网关地址 > " NEWGATEWAY
+read -p "请输入你要配置本机的计算机名 > " NEWNAME
+read -p "请输入你要配置本机的ip地址 > " NEWIP
+read -p "请输入你要配置本机的netmask地址 > " NEWNETMASK
+read -p "请输入你要配置本机的gateway地址 > " NEWGATEWAY
+read -p "请输入你要配置本机的dns地址 > " NEWDNS
 A=$(echo $NEWIP|cut -d. -f3)
 B=$(echo $NEWIP|cut -d. -f4)
 echo "请确认网络配置信息"
-echo "IP: $NEWIP  网关: $NEWGATEWAY"
+echo "name:$NEWNAME ip:$NEWIP netmask:$NEWNETMASK gateway:$NEWGATEWAY dns:$NEWDNS"
 choose
 }
 
